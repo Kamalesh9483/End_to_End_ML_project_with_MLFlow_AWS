dc1e3997d05cc6f1a71b135ed51b224aa2007bd6
Kamalesh <kamalesh9483@gmail.com>
2024-01-28 15:05:20 +0530

    Exception, Logger & Utils added

---
+++ main.py
@@ -0,0 +1,6 @@
+"""even this works, but to avoid src.mlProject, 
+there is a constructor file in mlProject folder which contains logger, which can be directly imported here"""
+# from src.mlProject import logger 
+
+from mlProject import logger
+logger.info('Custom logging')
