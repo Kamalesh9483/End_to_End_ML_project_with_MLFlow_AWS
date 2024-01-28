18baa1e0723060c5e8b51383e8cb7904fded97ea
Kamalesh <kamalesh9483@gmail.com>
2024-01-28 22:52:20 +0530

    data transformation added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ there is a constructor file in mlProject folder which contains logger, which can
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # logger.info('Custom logging')
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -21,6 +21,16 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
