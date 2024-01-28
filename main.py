f5f5c449f744be437e25dc84fc8b51339197cc69
Kamalesh <kamalesh9483@gmail.com>
2024-01-28 22:20:24 +0530

    data validation added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ there is a constructor file in mlProject folder which contains logger, which can
 
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 # logger.info('Custom logging')
 STAGE_NAME = "Data Ingestion stage"
@@ -15,3 +16,13 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
