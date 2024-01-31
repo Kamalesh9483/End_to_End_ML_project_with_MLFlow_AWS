3e865c825b740a0df5cc7c65f8857202c31c7bd0
Kamalesh <kamalesh9483@gmail.com>
2024-01-31 23:01:56 +0530

    code changed

--- app.py
+++ app.py
@@ -19,27 +19,27 @@ def index():
     if request.method == 'POST':
         try:
             # reading the inputs given by the user
-            # fixed_acidity = float(request.form['fixed_acidity'])
-            # volatile_acidity = float(request.form['volatile_acidity'])
-            # citric_acid = float(request.form['citric_acid'])
-            # residual_sugar = float(request.form['residual_sugar'])
-            # chlorides = float(request.form['chlorides'])
-            # free_sulfur_dioxide = float(request.form['free_sulfur_dioxide'])
-            # total_sulfur_dioxide = float(request.form['total_sulfur_dioxide'])
-            # density = float(request.form['density'])
-            # pH = float(request.form['pH'])
-            # sulphates = float(request.form['sulphates'])
-            # alcohol = float(request.form['alcohol'])
+            fixed_acidity = float(request.form['fixed_acidity'])
+            volatile_acidity = float(request.form['volatile_acidity'])
+            citric_acid = float(request.form['citric_acid'])
+            residual_sugar = float(request.form['residual_sugar'])
+            chlorides = float(request.form['chlorides'])
+            free_sulfur_dioxide = float(request.form['free_sulfur_dioxide'])
+            total_sulfur_dioxide = float(request.form['total_sulfur_dioxide'])
+            density = float(request.form['density'])
+            pH = float(request.form['pH'])
+            sulphates = float(request.form['sulphates'])
+            alcohol = float(request.form['alcohol'])
 
-            # data = [fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide,
-            #         total_sulfur_dioxide, density, pH, sulphates, alcohol]
-            # print(data)
-            # data = np.array(data).reshape(1, 11)
-            # print(data)
+            data = [fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide,
+                    total_sulfur_dioxide, density, pH, sulphates, alcohol]
+            print(data)
+            data = np.array(data).reshape(1, 11)
+            print(data)
 
-            # obj = PredictionPipeline()
-            # predict = obj.predict(data)
-            predict='test'
+            obj = PredictionPipeline()
+            predict = obj.predict(data)
+            # predict='test'
 
             return render_template('results.html', prediction=str(predict))
 
